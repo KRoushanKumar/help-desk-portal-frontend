64f7dfd03e2e6939812fe935183bd79f4b1a7d2e
Mr Alok <mralok369@gmail.com>
2023-05-02 11:55:30 +0530

    By Alok

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React from "react";
+import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+//import Axios from 'axios';
+import H from './pages/home.js'
+import Dashboard from "./components/Admin/pages/Dashboard";
+import About from "./components/Admin/pages/About";
+import Comments from "./components/Admin/pages/Comments";
+import Employees from "./components/Admin/pages/Employees";
+import Categories from "./components/Admin/pages/Categories";
+import Tickets from "./components/Admin/pages/Tickets";
+import Sidebar from "./components/Admin/Sidebar";
+function App() {
+
+  // learn withCredentials in Axios 
+  //Axios.defaults.withCredentials=true;
+  return (
+    <>
+      <Router>
+        <Sidebar>
+          <Routes>
+            <Route path="/" element={<H />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/employees" element={<Employees />} />
+            <Route path="/categories" element={<Categories />} />
+            <Route path="/tickets" element={<Tickets />} />
+            <Route path="/comments" element={<Comments />} />
+          </Routes>
+        </Sidebar>
+      </Router>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Employees/Pages/AboutEmp.js
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+
+const AboutEmp = () => {
+    return (
+        <div>
+            <h1>About Employee Page</h1>
+        </div>
+    );
+};
+
+export default AboutEmp;
