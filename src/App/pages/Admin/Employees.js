30f0d0238cc59f3b2c545cccbd883505596f8a66
KRoushanKumar <roushank559@gmail.com>
2023-05-04 21:55:17 +0530

    emp

--- src/App/pages/Admin/Employees.js
+++ src/App/pages/Admin/Employees.js
@@ -1,6 +1,15 @@
 import React, { useState } from 'react';
 
 const Employees = () => {
+
+
+    // function AddEmployee(adminId) {
+    //     Axios.post(`http://localhost:8080/api/AddEmployee/${adminId}`,{data:data}).then((res) => {
+    //         if (res.data)
+    //             alert("")
+    //     });
+    // }
+
     return (
         <div>
             <h1>Employees page</h1>
