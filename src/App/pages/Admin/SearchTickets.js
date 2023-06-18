1680947d8b6c556b85572027d142f2f9f2a896f7
AshuManit <ashutoshtigga2016@gmail.com>
2023-06-18 23:09:41 +0530

    updated searchemployee page

--- src/App/pages/Admin/SearchTickets.js
+++ src/App/pages/Admin/SearchTickets.js
@@ -190,11 +190,8 @@ const Categories = () => {
                                                                             <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                                                                         </div>
                                                                         <div class="modal-body p-2">
-                                                                            {
-                                                                                allEmpQuery.map((query) => (
                                                                                     <p >{query.description}</p>
-                                                                                ))
-                                                                            }
+                                                                             
                                                                         </div>
                                                                         <div class="modal-footer">
                                                                             <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
@@ -236,11 +233,7 @@ const Categories = () => {
                                                                             <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                                                                         </div>
                                                                         <div class="modal-body p-2">
-                                                                            {
-                                                                                allEmpQuery.map((query) => (
-                                                                                    <p >{query.description}</p>
-                                                                                ))
-                                                                            }
+                                                                                ....
                                                                         </div>
                                                                         <div class="modal-footer">
                                                                             <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
@@ -263,11 +256,7 @@ const Categories = () => {
                                                                             <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                                                                         </div>
                                                                         <div class="modal-body p-2">
-                                                                            {
-                                                                                allEmpQuery.map((query) => (
-                                                                                    <p >{query.description}</p>
-                                                                                ))
-                                                                            }
+                                                                           ...
                                                                         </div>
                                                                         <div class="modal-footer">
                                                                             <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
