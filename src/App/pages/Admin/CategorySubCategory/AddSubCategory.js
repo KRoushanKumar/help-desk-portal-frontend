fba866ecaf1f9bd237c28321d276d06f3a658dc7
AshuManit <ashutoshtigga2016@gmail.com>
2023-07-12 13:09:00 +0530

    UPDATED

--- src/App/pages/Admin/CategorySubCategory/AddSubCategory.js
+++ src/App/pages/Admin/CategorySubCategory/AddSubCategory.js
@@ -1,34 +1,16 @@
 import axios from "../../../../Assets/axios";
+import "../../../../Assets/css/admin/modal.css"
 
-
-const AddSubCategory=(props)=>{
-    
-    
-
-   console.log(props.ticCtgryName);
-   console.log(props.ticCtgryId);
-
+const AddSubCategory=({cancelShowSubCatModal})=>{
     const input=(e)=>{
-   
-        
-        var subcategoryName=document.getElementById('SubcategoryName').value;
-
-        axios.post(`addTicketSubCategories/${props.ticCtgryId}`,{name:subcategoryName})
-            .then(()=>{
-                alert(`Add successful ${subcategoryName}`)
-                console.log(subcategoryName)
-            }).catch((error) => { console.log(error) });
-    }
-
-    const hideAddSubCtgry = ()=>
-    {
-        props.showAddSubCategories(false);
+       
     }
 
     return(
         <div className="container-fluid">
         <div className="row">
-            <div className="col-md-4 m-auto mt-5"> 
+        <div className='modal-wrapper'></div>
+            <div className="col-md-4 m-auto mt-5 modal-container"> 
                 <div class="card text-dark">
                     <article class="card-body p-4">
 
@@ -38,7 +20,7 @@ const AddSubCategory=(props)=>{
 
                          <div class="form-group mb-2">
                                 <input name="" class="form-control"  disabled="disabled" placeholder=" category name" 
-                                       value={props.ticCtgryName} id="categoryName" type="text" required/>
+                                       value={""} id="categoryName" type="text" required/>
                             </div>
 
                             <div class="form-group mb-2">
@@ -46,8 +28,8 @@ const AddSubCategory=(props)=>{
                             </div>
 
                             <div class="form-group text-left">
-                                <button type="submit"  class="btn btn-success btn-block" onClick={()=>(input())}> Add  </button>
-                                <button type="submit" class="m-2 btn btn-danger btn-block" onClick={()=>(hideAddSubCtgry())}> Back  </button>
+                                <button  class="btn btn-success btn-block" onClick={()=>(input())}> Add  </button>
+                                <button  class="m-2 btn btn-danger btn-block" onClick={cancelShowSubCatModal}> Back  </button>
                             </div>
 
                         </div>
